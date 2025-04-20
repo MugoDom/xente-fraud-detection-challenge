c693b017a7ef7bbf8f0bef7b06cc2097dd7bbee2
MugoDom <njuemugodominic@gmail.com>
2025-04-20 15:44:18 +0300

    Add class weight to model parameters

--- src/model_training.py
+++ src/model_training.py
@@ -3,7 +3,15 @@ import joblib
 
 def train_model(X_train, y_train, model_path='random_forest_model.joblib'):
     # Train a Random Forest model
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42,
+    class_weight='balanced',  # this helps address class imbalance
+    max_depth=None,           # defaults can be tuned further with GridSearchCV
+    min_samples_split=2,
+    min_samples_leaf=1,
+    n_jobs=-1
+)
     model.fit(X_train, y_train)
 
     # Save the trained model
